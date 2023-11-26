13352f0ae19b28b7442898089f6bcc1ba38fc05d
jb-hslu <joel.birrer@stud.hslu.ch>
2023-11-26 13:00:37 +0100

    world map colored by number of medals

--- main.js
+++ main.js
@@ -20,72 +20,88 @@ document.addEventListener("DOMContentLoaded", function () {
     .attr("width", mapWidth)
     .attr("height", mapHeight);
 
-  // Load the world map data using TopoJSON
-  d3.json("https://unpkg.com/world-atlas@1.1.0/world/110m.json").then(
-    (worldData) => {
-      // Convert TopoJSON to GeoJSON
-      const countries = topojson.feature(
-        worldData,
-        worldData.objects.countries
-      );
+  Promise.all([
+    d3.json("Data/world_atlas_110m.json"),
+    d3.json("Data/data.json"),
+    d3.json("Data/country_codes.json"),
+  ]).then(([worldData, athletData, countryCodes]) => {
+     
+    // Create map to handle country ids
+     const map_country_id_name = new Map();
+     countryCodes.forEach((country) => {
+       map_country_id_name.set(country["country-code"], country["name"]);
+     });
+ 
+     // Create map to get amount of medals for each country
+     const map_country_medals = new Map();
+     athletData.forEach((athlet) => {
+       let country = athlet.Country;
+       let medal = athlet.Medal;
+       let count_medals = map_country_medals.get(country) || [0, 0, 0];
+       if (medal == "Gold") {
+         count_medals[0] += 1;
+       } else if (medal == "Silver") {
+         count_medals[1] += 1;
+       } else if (medal == "Bronze") {
+         count_medals[2] += 1;
+       }
+       map_country_medals.set(country, count_medals);
+     });
 
-      // Create a projection for the map
-      const projection = d3
-        .geoOrthographic()
-        .fitSize([mapWidth, mapHeight], countries);
+    const max_medals_weighted = Array.from(map_country_medals.values()).reduce(function(max, countryMedals) {
+      let val = weight_medals(countryMedals);
+      return val > max ? val : max;
+    }, 0);
+ 
+   const colorScale = d3.scaleLog().domain([1, max_medals_weighted]).range(["#f7f7f7", "#ffd700"]);
+ 
+    // Convert TopoJSON to GeoJSON
+    const countries = topojson.feature(
+      worldData,
+      worldData.objects.countries
+    );
 
-      // Create a path generator
-      const path = d3.geoPath().projection(projection);
+    // Create a projection for the map
+    const projection = d3
+      .geoOrthographic()
+      .fitSize([mapWidth, mapHeight], countries);
 
-      // Draw the map
-      const countriesPaths = svg
-        .selectAll("path")
-        .data(countries.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("stroke", "#fff")
-        .attr("fill", "#ccc")
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
+    // Create a path generator
+    const path = d3.geoPath().projection(projection);
 
-      let rotationTimer;
+    // Draw the map
+    const countriesPaths = svg
+      .selectAll("path")
+      .data(countries.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("stroke", "#fff")
+      .attr("fill", (d) => {
+        let countryName = map_country_id_name.get(d.id);
+        let medals = map_country_medals.get(countryName) || [0, 0, 0];
+        return colorScale(weight_medals(medals));
+      })
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut)
+      .on("click", handleClick);
 
-      // Function to handle mouseover events
-      function handleMouseOver() {
-        d3.select(this).attr("fill", "#3498db");
-        // Pause the continuous rotation when the mouse enters the globe
-        clearInterval(rotationTimer);
-      }
+    let rotationTimer;
 
-      // Function to handle mouseout events
-      function handleMouseOut() {
-        d3.select(this).attr("fill", "#ccc");
-        // Resume the continuous rotation when the mouse leaves the globe
-        rotationTimer = setInterval(() => {
-          const rotate = projection.rotate();
-          rotate[0] += 0.1; // Adjust the rotation speed
-          projection.rotate(rotate);
-          countriesPaths.attr("d", path);
-        }, 30); // Adjust the interval
-      }
+    // Function to handle mouseover events
+    function handleMouseOver() {
+      let tempColor = this.getAttribute("fill");
+      d3.select(this).attr("fill", "#3498db");
+      d3.select(this).attr("tempColor", tempColor);
+      // Pause the continuous rotation when the mouse enters the globe
+      clearInterval(rotationTimer);
+    }
 
-      // Enable dragging on the map
-      svg.call(
-        d3
-          .drag()
-          .subject(() => {
-            const r = projection.rotate();
-            return { x: r[0], y: -r[1] };
-          })
-          .on("drag", () => {
-            const rotate = projection.rotate();
-            projection.rotate([d3.event.x, -d3.event.y, rotate[2]]);
-            countriesPaths.attr("d", path);
-          })
-      );
-
-      // Start the continuous rotation
+    // Function to handle mouseout events
+    function handleMouseOut() {
+      let color = this.getAttribute("tempColor");
+      d3.select(this).attr("fill", color);
+      // Resume the continuous rotation when the mouse leaves the globe
       rotationTimer = setInterval(() => {
         const rotate = projection.rotate();
         rotate[0] += 0.1; // Adjust the rotation speed
@@ -93,5 +109,59 @@ document.addEventListener("DOMContentLoaded", function () {
         countriesPaths.attr("d", path);
       }, 30); // Adjust the interval
     }
-  );
+
+    function handleClick(d) {
+      let countryName = map_country_id_name.get(d.id);
+
+      console.log(countryName);
+
+      let country_information = d3.select("#country-overview").html("Land: " + countryName);
+      country_information.selectAll("*").remove();
+
+      if(map_country_medals.get(countryName) == undefined) {
+        return;
+      }
+
+      console.log(map_country_medals.get(countryName));
+
+      let ul = country_information.append("ul");
+      ul.selectAll("li").data(map_country_medals.get(countryName)).enter().append("li").text(function(amount_medal, index){
+        if (index == 0) {
+          return `🥇: ${amount_medal}`;
+        }
+        else if (index == 1) {
+          return `🥈: ${amount_medal}`;
+        }
+        else if (index == 2) {
+          return `🥉: ${amount_medal}`;
+        }
+    })};
+
+    // Enable dragging on the map
+    svg.call(
+      d3
+        .drag()
+        .subject(() => {
+          const r = projection.rotate();
+          return { x: r[0], y: -r[1] };
+        })
+        .on("drag", () => {
+          const rotate = projection.rotate();
+          projection.rotate([d3.event.x, -d3.event.y, rotate[2]]);
+          countriesPaths.attr("d", path);
+        })
+    );
+
+    // Start the continuous rotation
+    rotationTimer = setInterval(() => {
+      const rotate = projection.rotate();
+      rotate[0] += 0.1; // Adjust the rotation speed
+      projection.rotate(rotate);
+      countriesPaths.attr("d", path);
+    }, 30); // Adjust the interval
+  });
 });
+
+function weight_medals(medals) {
+  return medals[0] * 3 + medals[1] * 2 + medals[2] * 1;
+}
