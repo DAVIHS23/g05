b6b1ceae069a5e0a4a1feb7d15224be70d00c83f
phwalczak <95573786+phwalczak@users.noreply.github.com>
2023-11-27 22:03:00 +0100

    adjustments dataprep, first graph and structure changes

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const mapContainer = d3.select("#map-container");
   const graphsContainer = d3.select("#graphs-container");
 
-  // Set the initial width of graphsContainer to 0
-  graphsContainer.style("width", "0");
+  let rotationTimer;
 
   mapContainer.on("click", function () {
-    graphsContainer.transition().duration(100).style("width", "100%");
+    // Move the map to the left when clicked
+    mapContainer.transition().duration(50).style("margin-left", "0");
+    // Expand the graphs container
+    graphsContainer.transition().duration(500).style("width", "100%");
+
+    //svg.attr("height", mapContainer.node().getBoundingClientRect().height);
+
+    clearInterval(rotationTimer);
   });
 
   // Get the dimensions of the map container
@@ -25,41 +31,43 @@ document.addEventListener("DOMContentLoaded", function () {
     d3.json("Data/data.json"),
     d3.json("Data/country_codes.json"),
   ]).then(([worldData, athletData, countryCodes]) => {
-     
     // Create map to handle country ids
-     const map_country_id_name = new Map();
-     countryCodes.forEach((country) => {
-       map_country_id_name.set(country["country-code"], country["name"]);
-     });
- 
-     // Create map to get amount of medals for each country
-     const map_country_medals = new Map();
-     athletData.forEach((athlet) => {
-       let country = athlet.Country;
-       let medal = athlet.Medal;
-       let count_medals = map_country_medals.get(country) || [0, 0, 0];
-       if (medal == "Gold") {
-         count_medals[0] += 1;
-       } else if (medal == "Silver") {
-         count_medals[1] += 1;
-       } else if (medal == "Bronze") {
-         count_medals[2] += 1;
-       }
-       map_country_medals.set(country, count_medals);
-     });
+    const map_country_id_name = new Map();
+    countryCodes.forEach((country) => {
+      map_country_id_name.set(country["country-code"], country["name"]);
+    });
 
-    const max_medals_weighted = Array.from(map_country_medals.values()).reduce(function(max, countryMedals) {
-      let val = weight_medals(countryMedals);
-      return val > max ? val : max;
-    }, 0);
- 
-   const colorScale = d3.scaleLog().domain([0.01, max_medals_weighted]).range(["#f7f7f7", "#ffd700"]);
- 
-    // Convert TopoJSON to GeoJSON
-    const countries = topojson.feature(
-      worldData,
-      worldData.objects.countries
+    // Create map to get amount of medals for each country
+    const map_country_medals = new Map();
+    athletData.forEach((athlet) => {
+      let country = athlet.Country;
+      let medal = athlet.Medal;
+      let count_medals = map_country_medals.get(country) || [0, 0, 0];
+      if (medal == "Gold") {
+        count_medals[0] += 1;
+      } else if (medal == "Silver") {
+        count_medals[1] += 1;
+      } else if (medal == "Bronze") {
+        count_medals[2] += 1;
+      }
+      map_country_medals.set(country, count_medals);
+    });
+
+    const max_medals_weighted = Array.from(map_country_medals.values()).reduce(
+      function (max, countryMedals) {
+        let val = weight_medals(countryMedals);
+        return val > max ? val : max;
+      },
+      0
     );
+
+    const colorScale = d3
+      .scaleLog()
+      .domain([0.01, max_medals_weighted])
+      .range(["#f7f7f7", "#ffd700"]);
+
+    // Convert TopoJSON to GeoJSON
+    const countries = topojson.feature(worldData, worldData.objects.countries);
 
     // Create a projection for the map
     const projection = d3
@@ -86,8 +94,6 @@ document.addEventListener("DOMContentLoaded", function () {
       .on("mouseout", handleMouseOut)
       .on("click", handleClick);
 
-    let rotationTimer;
-
     // Function to handle mouseover events
     function handleMouseOver() {
       let tempColor = this.getAttribute("fill");
@@ -111,28 +117,105 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function handleClick(d) {
+      // Remove the "selected" class from previously selected countries
+      countriesPaths.classed("selected-country", false);
+
       let countryName = map_country_id_name.get(d.id);
+
+      d3.select(this).classed("selected-country", true);
 
       console.log(countryName);
 
-      let country_information = d3.select("#country-overview").html("Land: " + countryName);
+      let country_information = d3
+        .select("#country-overview")
+        .html("Land: " + countryName);
       country_information.selectAll("*").remove();
 
       console.log(map_country_medals.get(countryName));
-      countryMedals = map_country_medals.get(countryName) || [0, 0, 0] // Prevent a country from having no data
+      countryMedals = map_country_medals.get(countryName) || [0, 0, 0]; // Prevent a country from having no data
 
       let ul = country_information.append("ul");
-      ul.selectAll("li").data(countryMedals).enter().append("li").text(function(amount_medal, index){
-        if (index == 0) {
-          return `🥇: ${amount_medal}`;
-        }
-        else if (index == 1) {
-          return `🥈: ${amount_medal}`;
-        }
-        else if (index == 2) {
-          return `🥉: ${amount_medal}`;
-        }
-    })};
+      ul.selectAll("li")
+        .data(countryMedals)
+        .enter()
+        .append("li")
+        .text(function (amount_medal, index) {
+          if (index == 0) {
+            return `🥇: ${amount_medal}`;
+          } else if (index == 1) {
+            return `🥈: ${amount_medal}`;
+          } else if (index == 2) {
+            return `🥉: ${amount_medal}`;
+          }
+        });
+
+      // Calculate the maximum gold medals across all countries
+      const maxGoldMedals = d3.max(
+        Array.from(map_country_medals.values()).map(
+          (countryMedals) => countryMedals[0]
+        )
+      );
+
+      // Define a scale based on the maximum gold medals
+      const yScale = d3.scaleLinear().domain([0, 3000]).range([200, 0]); // Adjust the range as needed
+
+      // Create a bar chart
+      const barWidth = 500 / 3; // Divide by the number of medal types
+      const barSpacing = 10;
+
+      const barChart = d3
+        .select("#country-overview")
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", 250); // Increased height to accommodate axes
+
+      // Add y-axis
+      const yAxis = d3.axisLeft(yScale);
+      barChart
+        .append("g")
+        .attr("class", "y-axis") // Added class for styling
+        .attr("transform", "translate(40,0)") // Adjust the position as needed
+        .call(yAxis);
+
+      // Add bars for gold, silver, and bronze
+      const medalColors = ["#ffd700", "#c0c0c0", "#cd7f32"];
+      const medalLabels = ["Gold", "Silver", "Bronze"];
+
+      barChart
+        .selectAll("rect")
+        .data(countryMedals)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => i * (barWidth + barSpacing) + 40) // Adjust the position to align with the y-axis
+        .attr("y", (d) => yScale(d))
+        .attr("width", barWidth)
+        .attr("height", (d) => 200 - yScale(d))
+        .attr("fill", (d, i) => medalColors[i]);
+
+      // Add x-axis labels
+      barChart
+        .selectAll(".x-label")
+        //.data(medalLabels)
+        .enter()
+        .append("text")
+        .attr("class", "x-label")
+        .attr("x", (d, i) => i * (barWidth + barSpacing) + 40 + barWidth / 2) // Center the label under each bar
+        .attr("y", 220) // Adjust the position as needed
+        .attr("text-anchor", "middle")
+        .text((d) => d);
+
+      // Get athletes data for the clicked country
+      const countryAthletes = athletData.filter(
+        (athlete) => athlete.Country === countryName
+      );
+
+      // Log the names of athletes for the selected country
+      countryAthletes.forEach((athlete) => {
+        console.log(athlete.Name);
+      });
+
+      clearInterval(rotationTimer);
+    }
 
     // Enable dragging on the map
     svg.call(
